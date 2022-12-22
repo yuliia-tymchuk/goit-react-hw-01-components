719394812d08ddf33ac33f85854a0cbab9ddc9ca
lia2307 <juliatymchuk23@gmail.com>
2022-12-22 23:58:01 +0200

    part of t2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,20 @@
 import { Profile } from './profile/Profile';
 import user from './data/user.json';
 
+import { Statistics } from './statistics/Statistics';
+import data from './data/data.json';
+
 export const App = () => {
   return (
     <div>
       <Profile
-        title="profile"
         username={user.username}
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };

11061c7d3ce417f67e096223ffa2bd6c11b40938
lia2307 <juliatymchuk23@gmail.com>
2022-12-22 22:36:44 +0200

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Profile } from './profile/Profile';
-import user from '../components/profile/user.json';
+import user from './data/user.json';
 
 export const App = () => {
   return (
     <div>
       <Profile
+        title="profile"
         username={user.username}
         tag={user.tag}
         location={user.location}
